e22fe3a1ec2e1813c170c2c30629b57e711b85f9
Max <lomaka.maxim@gmail.com>
2018-06-12 16:05:35 +0300

    add login

--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from store.views import *
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', AdListView.as_view(), name='index'),
     path('<int:pk>/', AdDetailView.as_view(), name='detail'),
     path('registration/', CreateUserView.as_view(), name='registration'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html')),
 ]
--- store/views.py
+++ store/views.py
@@ -1,11 +1,13 @@
 from django.contrib import messages
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, FormView
 from rest_framework.viewsets import ModelViewSet
 
 from store.forms import UserForm
-from store.models import Advertisement, User
+from store.models import Advertisement
 from store.serializers import AdSerializer, UserSerializer
 
 
@@ -26,14 +28,25 @@ class CreateUserView(FormView):
     success_url = reverse_lazy('store:index')
 
     def form_valid(self, form):
-        password = make_password(form['password'].value())
-        print(password)
-        form.password = password
-        user = form.save(form)
-
+        print(form)
+        # password = make_password(form['password'].value())
+        # print(password)
+        # form.password = password
+        form.save()
         messages.success(self.request, 'user was created successfully')
 
         return super().form_valid(form)
+
+    '''sign in view for log in user'''
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'store/login.html')
 
 
 '''viewsets for serializers'''
